6f85f3a9b2443c960be0126d4324cdf09b42047c
kviliev <kalin.viliev@gmail.com>
2023-02-21 16:28:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/kvilievorg/s3-webappapp/aws"
+  source  = "app.terraform.io/kviliev/terraform-aws-s3-webappapp/aws"
   name    = var.name
   region  = var.region
   prefix  = var.prefix
